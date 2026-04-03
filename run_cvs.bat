f6f190a985b2b1244b76b2807347e3eb53fc4931
Aneesh Pamula <aneesh4942@gmail.com>
2026-04-03 16:47:13 -0700

    Update run_cvs.bat

--- run_cvs.bat
+++ run_cvs.bat
@@ -1,4 +1,9 @@
+@echo off
+
+REM Before running this script, ensure "coronado_hills_data.csv", "coronado_hills_full.json", and "training_set_cameras_data.csv" are in the camera_data directory.
+REM Then, run this script by running "run_cvs.bat" in the terminal. You may need to type "./run_cvs.bat" if an error occurs.
+
 python run_cross_validation.py --objective "triplet"
-python run_cross_validation.py --objective "deepsad"
-python run_cross_validation.py --objective "hsad"
-python run_cross_validation.py --objective "final"
+python run_cross_validation.py --objective "deepsad" --data-csv-name "coronado_hills_full.json"
+python run_cross_validation.py --objective "hsad" --data-csv-name "coronado_hills_full.json"
+python run_cross_validation.py --objective "final" --data-csv-name "training_set_cameras_data.csv"
